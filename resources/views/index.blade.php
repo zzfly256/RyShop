0fa5f7167b60ccedc98f621a4b22fd074e013f2c
zzfly256 <admin@zzfly.net>
2017-08-13 22:40:58 +0800

    修改 主页菜单栏效果

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,47 @@
     口袋主机 - Host in Pocket，提供高速虚拟主机、优质VPS服务，涵盖免费空间、香港空间、美国空间、高防空间
 @stop
 @include('header')
-@include('nav')
+
+<div class="navbar-background index_menu">
+    <section class="grid-header container grid-960">
+        <header class="navbar">
+            <section class="navbar-section">
+                <a href="/" class="navbar-brand">{{\App\Setings::whereRaw("name='siteName'")->first()->value}}</a>
+                <a href="/host" class="btn btn-link">主机列表</a>
+                <a href="/news" class="btn btn-link">新闻公告</a>
+                @if(Auth::user())
+                    <a href="/my_host" class="btn btn-link">我的主机</a>
+                    <a href="/my_order" class="btn btn-link">我的订单</a>
+                    @if(Auth::user()->level==0)
+                        <a href="/admin" class="btn btn-link">管理员面板</a>
+                    @endif
+                @endif
+            </section>
+            <section class="navbar-section">
+                <div class="input-group input-inline">
+                    @if(Auth::user())
+                        <div class="dropdown">
+                            <a href="#" class="btn btn-link dropdown-toggle" tabindex="0">
+                                欢迎您：<?php echo Auth::user()->name;?><i class="icon icon-caret"></i>
+                            </a>
+                            <!-- menu component -->
+                            <ul class="menu">
+                                <li class="menu-item"><a href="/home" class="btn btn-link">个人资料</a></li>
+                                <li class="menu-item"><a href="/auth/logout" class="btn btn-link">退出</a></li>
+                            </ul>
+                        </div>
+
+                    @else
+                        <a href="/auth/login" class="btn btn-link">登录</a>
+                        <a href="/auth/register" class="btn btn-link">注册</a>
+                    @endif
+                </div>
+            </section>
+        </header>
+    </section>
+</div>
+
+
 @if(!empty(\App\Setings::whereRaw("name='index_img'")->first()->value))
     <img class="index_img" src="{{\App\Setings::whereRaw("name='index_img'")->first()->value}}" alt="{{\App\Setings::whereRaw("name='siteName'")->first()->value}}">
 @endif
